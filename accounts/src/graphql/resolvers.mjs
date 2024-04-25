c3c9412d194ebe61c385f80939f3cc3e3fb113ad
Jeremy <jeremy.dryden@gmail.com>
2024-04-24 22:01:36 -0400

    working on account resolvers

--- accounts/src/graphql/resolvers.mjs
+++ accounts/src/graphql/resolvers.mjs
@@ -10,14 +10,41 @@ const accounts = [
 const resolvers = {
   Account: {
     __resolveReference(reference) {
-      return accounts.find((account) => account.id === reference.id);
+      return auth0.users.get({ id: reference.id });
+    },
+    id(account) {
+      return account.user_id;
+    },
+    createdAt(account) {
+      return account.created_at;
     },
   },
+
   Query: {
+    async account(root, { id }) {
+      const account = await auth0.users.get({ id });
+
+      return account.data;
+    },
+
+    async accounts() {
+      console.log("test");
+      const accounts = await auth0.users.get();
+      console.log("ACC:", accounts);
+      // return accounts.data;
+
+      return auth0.getUsers({ per_page: 10, page: 1 }, function (err, users) {
+        console.log(users.length);
+      });
+    },
+
     async viewer(parent, args, { user }) {
-      const viewer = await auth0.users.get({ id: user.sub });
-      // console.log("RESOLVER->USER: ", viewer);
-      return accounts[0];
+      if (user?.sub) {
+        const viewer = await auth0.users.get({ id: user.sub });
+
+        return viewer.data;
+      }
+      return null;
     },
   },
 };
