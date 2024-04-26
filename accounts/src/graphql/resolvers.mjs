2c97fabeecaab7464a1742a5a3757d80424849e0
Jeremy <jeremy.dryden@gmail.com>
2024-04-25 23:00:14 -0400

    getAll() still doesn't work, need a way to get all the users

--- accounts/src/graphql/resolvers.mjs
+++ accounts/src/graphql/resolvers.mjs
@@ -1,12 +1,5 @@
 import auth0 from "../config/auth0.mjs";
 
-const accounts = [
-  {
-    id: "1",
-    email: "marked@mandiwise.com",
-  },
-];
-
 const resolvers = {
   Account: {
     __resolveReference(reference) {
@@ -27,15 +20,16 @@ const resolvers = {
       return account.data;
     },
 
-    async accounts() {
-      console.log("test");
-      const accounts = await auth0.users.get();
+    async accounts(root, args) {
+      // console.log(auth0.clients.getAll());
+      const accounts = await auth0.clients.get();
+      // const accounts = await auth0.users.get();
       console.log("ACC:", accounts);
-      // return accounts.data;
+      return accounts.data;
 
-      return auth0.getUsers({ per_page: 10, page: 1 }, function (err, users) {
-        console.log(users.length);
-      });
+      // return auth0.getUsers({ per_page: 10, page: 1 }, function (err, users) {
+      //   console.log(users.length);
+      // });
     },
 
     async viewer(parent, args, { user }) {
