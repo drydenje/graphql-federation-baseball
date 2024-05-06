0c030293eccb88129f7609ff2e528f29a3c99696
Jeremy <jeremy.dryden@gmail.com>
2024-05-05 23:05:48 -0400

    more authorize work

---
+++ shared/src/directives/authDirectives.mjs
@@ -0,0 +1,53 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { readFileSync } from "fs";
+
+import { ApolloServerErrorCode } from "@apollo/server/errors";
+import { defaultFieldResolver } from "graphql";
+import { getDirectives, MapperKind, mapSchema } from "@graphql-tools/utils";
+
+function authDirectives() {
+  /* 
+    Return an object containing the auth-related 
+    directive definitions plus a function that 
+    will transform Object type fields in the 
+    subgraph schema to handle the directives, 
+    where applied
+  */
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+
+  return {
+    authDirectivesTypeDefs: readFileSync(
+      resolve(__dirname, "./authDirectives.graphql"),
+      "utf-8"
+    ),
+    authDirectivesTransformer: (schema) =>
+      mapSchema(schema, {
+        [MapperKind.OBJECT_FIELD]: (fieldConfig) => {
+          // Directive handling logic will go here...
+          const fieldDirectives = getDirectives(schema, fieldConfig);
+          const privateDirective = fieldDirectives.find(
+            (dir) => dir.name === "private"
+          );
+
+          const { resolve = defaultFieldResolver } = fieldConfig;
+
+          if (privateDirective) {
+            fieldConfig.resolve = function (source, args, context, info) {
+              const privateAuthorized = privateDirective && context.user?.sub;
+
+              if (!privateAuthorized) {
+                throw new ApolloServerErrorCode("Not authorized!");
+              }
+
+              return resolve(source, args, context, info);
+            };
+
+            return fieldConfig;
+          }
+        },
+      }),
+  };
+}
+
+export default authDirectives;
